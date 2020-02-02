d365f0e2b4ab952136cfd64a53b5743400dee37e
Your Name <amit.pinaki@gmail.com>
2020-02-02 10:21:04 +0530

    Fixed formatting

--- 001_facial_keypoint/data_load.py
+++ 001_facial_keypoint/data_load.py
@@ -29,13 +29,13 @@ class FacialKeypointsDataset(Dataset):
     def __getitem__(self, idx):
         image_name = os.path.join(self.root_dir,
                                 self.key_pts_frame.iloc[idx, 0])
-        
+
         image = mpimg.imread(image_name)
-        
+
         # if image has an alpha color channel, get rid of it
         if(image.shape[2] == 4):
             image = image[:,:,0:3]
-        
+
         key_pts = self.key_pts_frame.iloc[idx, 1:].as_matrix()
         key_pts = key_pts.astype('float').reshape(-1, 2)
         sample = {'image': image, 'keypoints': key_pts}
@@ -44,27 +44,27 @@ class FacialKeypointsDataset(Dataset):
             sample = self.transform(sample)
 
         return sample
-    
 
-    
+
+
 # tranforms
 
 class Normalize(object):
-    """Convert a color image to grayscale and normalize the color range to [0,1]."""        
+    """Convert a color image to grayscale and normalize the color range to [0,1]."""
 
     def __call__(self, sample):
         image, key_pts = sample['image'], sample['keypoints']
-        
+
         image_copy = np.copy(image)
         key_pts_copy = np.copy(key_pts)
 
         # convert image to grayscale
         image_copy = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        
+
         # scale color range from [0, 255] to [0, 1]
         image_copy=  image_copy/255.0
-            
-        
+
+
         # scale keypoints to be centered around 0 with a range of [-1, 1]
         # mean = 100, sqrt = 50, so, pts should be (pts - 100)/50
         key_pts_copy = (key_pts_copy - 100)/50.0
@@ -101,7 +101,7 @@ class Rescale(object):
         new_h, new_w = int(new_h), int(new_w)
 
         img = cv2.resize(image, (new_w, new_h))
-        
+
         # scale the pts, too
         key_pts = key_pts * [new_w / w, new_h / h]
 
@@ -146,16 +146,16 @@ class ToTensor(object):
 
     def __call__(self, sample):
         image, key_pts = sample['image'], sample['keypoints']
-         
+
         # if image has no grayscale color channel, add one
         if(len(image.shape) == 2):
             # add that third color dim
             image = image.reshape(image.shape[0], image.shape[1], 1)
-            
+
         # swap color axis because
         # numpy image: H x W x C
         # torch image: C X H X W
         image = image.transpose((2, 0, 1))
-        
+
         return {'image': torch.from_numpy(image),
                 'keypoints': torch.from_numpy(key_pts)}
